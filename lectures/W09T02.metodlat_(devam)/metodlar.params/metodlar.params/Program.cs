6fd4473f3a1b6e2f3219ed8eb0cc20ba5e003322
coshkun <cskncnr@gmail.com>
2017-05-02 23:23:38 +0300

    update

---
+++ lectures/W09T02.metodlat_(devam)/metodlar.params/metodlar.params/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metodlar.@params
+{
+    class Program
+    {
+        static List<float> parametreler = new List<float>();
+
+        static void Main(string[] args)
+        {
+            int say = 0;
+            Console.WriteLine("Ortalama almak için \"Ortalama\" komutunu giriniz, ardından parametreleri giriniz.");
+            string cmd = Console.ReadLine();
+
+            Console.WriteLine((++say).ToString() +".Sayıyı giriniz.(Hesaplamak için 'h' giriniz)");
+            while (say > 0)
+            {
+                string input = Console.ReadLine().Trim();
+                if (input.Trim().ToLower() == "h")
+                    break;
+
+                parametreler.Add(Convert.ToSingle(input));
+            }
+
+            float? sonuc = ortala(cmd, parametreler.ToArray());
+            if(sonuc != null)
+                Console.WriteLine("Sonuc= " + sonuc);
+            else
+                Console.WriteLine("Sonuc hesaplanamıyor..");
+            Console.ReadKey();
+        }
+
+        public static float? ortala(string cmd, params float[] args)
+        {
+            if (cmd.Trim().ToLower() != "ortalama")
+                return null;
+
+            float toplam = 0, snc =0;
+            foreach (float i in args)
+            {
+                toplam += i;
+                snc = toplam / (float)args.Length;
+            }
+
+            return snc;
+        }
+    }
+}
---
+++ lectures/W09T02.metodlat_(devam)/metodlar.recursive/metodlar.recursive/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metodlar.recursive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Faktoriyel alınacak sayı gir: ");
+            var say = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Sonuc= " + iterFaktoriyel(say));
+            Console.ReadKey();
+        }
+
+        public static int faktoriyel(int x) // Rekürsif model
+        {
+            if (x == 0)
+                return 1;
+
+            return x * faktoriyel(x - 1);
+        }
+
+        public static int iterFaktoriyel(int x) // İteratif model
+        {
+            int snc = 1;
+            for (int i = 1; i <= x; i++)
+            {
+                snc *= i;
+            }
+            return snc;
+        }
+    }
+}
